6e7941fdffbe9b29331cfa498f8bca07009d2259
Luiz Almada <luizalmada@yahoo.com>
2024-10-19 23:56:02 -0300

    Customiza validacao assincrona

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,24 @@ import 'dotenv/config';
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  //Configuração do PIPE do class-validator
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+
+  //Informa para o class-validator (useContainer) utilizar a mesma resolução de dependências do NestJs
+  useContainer(app.select(AppModule), {fallbackOnErrors: true});
+
   await app.listen(process.env.PORT ?? 3000, ()=>console.log(`Servidor escutando na porta: ${process.env.PORT ?? 3000}`));
 }
 bootstrap();
---
+++ src/usuario/dto/CriaUsuario.dto.ts
@@ -0,0 +1,22 @@
+import {
+  IsDefined,
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+} from 'class-validator';
+import { EmailEhUnico } from '../validacao/email-eh-unico.validator';
+
+export class CriaUsuarioDTO {
+  //@IsString()
+  @IsNotEmpty({ message: 'O nome não pode ser vazio' }) //Verifica também o IsDefined
+  //@IsDefined()
+  nome: string;
+
+  @IsEmail(undefined, { message: 'O email informado é inválido' })
+  @EmailEhUnico({message: 'Já existe um usuário com este e-mail'})
+  email: string;
+
+  @MinLength(6, { message: 'A senha precisa ter pelo menos 6 caracteres' })
+  senha: string;
+}

ae5d084130f3a9b1dd2ea1fe02636d60025d1ce5
Luiz Almada <luizalmada@yahoo.com>
2024-10-21 22:48:26 -0300

    Instala e Configura o NestJS/TypeORM e o NestJS/Config e Instala o Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
 import { UsuarioModule } from './usuario/usuario.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigService } from './config/postgres.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsuarioModule],
+  imports: [
+    UsuarioModule,
+    //Configura o Config do NestJS
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    //Configura TypeOrmModule
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService]
+    })
+  ],
 })
 export class AppModule {}

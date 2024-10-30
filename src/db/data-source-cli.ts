fd50976c965cf53394884adcf34cc695cae95d1d
Luiz Almada <luizalmada@yahoo.com>
2024-10-30 08:32:30 -0300

    Ajusta Código para o Curso: Lidando com migrações, relacionamentos ORM e erros em uma API

--- src/db/data-source-cli.ts
+++ src/db/data-source-cli.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { DataSource, DataSourceOptions  } from 'typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
 
 const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
@@ -8,8 +8,10 @@ const dataSourceOptions: DataSourceOptions = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: [__dirname + '/../**/*.entity{.js,.ts}'],
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
   migrations: [__dirname + '/migrations/*.{js,ts}'],
 };
+
 const dataSource = new DataSource(dataSourceOptions);
+
 export default dataSource;
